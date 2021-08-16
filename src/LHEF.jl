f4b5bb3e905dc27e73c6405f2e34c91497b7ade3
Nick Amin <amin.nj@gmail.com>
2021-08-16 16:53:50 -0700

    simplify

--- src/LHEF.jl
+++ src/LHEF.jl
@@ -1,87 +1,52 @@
 module LHEF
 
-using LightXML, LorentzVectors
+using EzXML, LorentzVectors
 
 export parse_lhe
 
-struct EventHeader
-    nup::UInt8          # Number of particles
-    ldprup::UInt8       # Process type?
-    xwgtup::Float64     # Event Wight
-    scalup::Float64     # Scale
-    αem::Float64        # AQEDUP
-    αs::Float64         # AQCDUP
-end
-
-struct Particle
-    particle::Int32
-    status::Int8
-    mothup::NTuple{2,UInt8}
-    color::NTuple{2,UInt16}
-    pμ::LorentzVector{Float64}
-    m::Float64
-    vtimup::Float64
-    spinup::Float64
-end
-
-struct Event
-    header::EventHeader
-    data::Vector{Particle}
-end
-
 function parse_lhe(filename; format=nothing)
-    if format === nothing
-        # Format not declared, inferring from extension
-        fparts = split(basename(filename), ".")
-        format = if fparts[end] == "lhe"
-            :lhe
-        elseif fparts[end] == "gz" && fparts[end - 1] == "lhe"
-            :lhegz
-        end
-    end
+    events = []
+    reader = open(EzXML.StreamReader, filename)
+    while (item = iterate(reader)) != nothing
+        (reader.type != EzXML.READER_ELEMENT) && continue
+        (reader.name != "event") && continue
 
-    lhefile = parse_file(filename)
-    lhenode = root(lhefile)
-
-    (name(lhenode) == "LesHouchesEvents") || error("Invalid root node")
-    events = lhenode["event"]
-    return [
-        begin
-            data = content(first(child_nodes(event)))
-            lines = split(data, '\n'; keepempty=false)
-            headerdata = split(lines[1], ' '; keepempty=false)
-            header = EventHeader(
-                parse(UInt8, headerdata[1]),
-                parse(UInt8, headerdata[2]),
-                parse(Float64, headerdata[3]),
-                parse(Float64, headerdata[4]),
-                parse(Float64, headerdata[5]),
-                parse(Float64, headerdata[6]),
-            )
-            data = [
-                begin
-                    fields = split(line, ' '; keepempty=false)
-                    p = Particle(
-                        parse(Int32, fields[1]),
-                        parse(Int8, fields[2]),
-                        (parse(UInt8, fields[3]), parse(UInt8, fields[4])),
-                        (parse(UInt16, fields[5]), parse(UInt16, fields[6])),
-                        LorentzVector(
-                            parse(Float64, fields[10]),
-                            parse(Float64, fields[7]),
-                            parse(Float64, fields[8]),
-                            parse(Float64, fields[9]),
-                        ),
-                        parse(Float64, fields[11]),
-                        parse(Float64, fields[12]),
-                        parse(Float64, fields[13]),
-                    )
-                    p
-                end for line in lines[2:end]
-            ]
-            Event(header, data)
-        end for event in events
-    ]
+        lines = split(reader.content, '\n'; keepempty=false)
+        headerdata = split(lines[1], ' '; keepempty=false)
+        header = (;
+            nup=parse(UInt8, headerdata[1]),       # Number of particles
+            ldprup=parse(UInt8, headerdata[2]),    # Process type
+            xwgtup=parse(Float64, headerdata[3]),  # Event weight
+            xscalup=parse(Float64, headerdata[4]), # Scale
+            αem=parse(Float64, headerdata[5]),     # AQEDUP
+            αs=parse(Float64, headerdata[6]),      # AQCDUP
+        )
+        particles = [
+            begin
+                fields = split(line, ' '; keepempty=false)
+                p = (;
+                    particle=parse(Int32, fields[1]),
+                    status=parse(Int8, fields[2]),
+                    mothup=(parse(UInt8, fields[3]), parse(UInt8, fields[4])),
+                    color=(parse(UInt16, fields[5]), parse(UInt16, fields[6])),
+                    p4=LorentzVector(
+                        parse(Float64, fields[10]),
+                        parse(Float64, fields[7]),
+                        parse(Float64, fields[8]),
+                        parse(Float64, fields[9]),
+                    ),
+                    m=parse(Float64, fields[11]),
+                    vtimup=parse(Float64, fields[12]),
+                    spinup=parse(Float64, fields[13]),
+                )
+                p
+            end for line in lines[2:2+header.nup-1]
+        ]
+        # Right now this is not lazy
+        # figure out how to do `yield blah` in julia
+        push!(events, (; header=header, particles=particles))
+   end
+   return events
 end
 
 end # module
