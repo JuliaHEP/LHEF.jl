d6520a7f62dff829ba4cda39c3d7bc7d4fc70e5f
Nick Amin <amin.nj@gmail.com>
2021-08-16 19:38:41 -0700

    repr and rename variables

--- src/LHEF.jl
+++ src/LHEF.jl
@@ -4,39 +4,54 @@ using EzXML, LorentzVectors
 
 export parse_lhe
 
+struct Event
+    header
+    particles
+end
+
+function Base.show(io::IO, evt::Event)
+    println()
+    println("  Event header: ", evt.header)
+    println("  Event particles:")
+    for p in evt.particles
+        println("    ", p)
+    end
+end
+
 function parse_event(event)
     lines = split(event, '\n'; keepempty=false)
     headerdata = split(lines[1], ' '; keepempty=false)
     header = (;
-        nup=parse(UInt8, headerdata[1]),       # Number of particles
-        ldprup=parse(UInt8, headerdata[2]),    # Process type
-        xwgtup=parse(Float64, headerdata[3]),  # Event weight
-        xscalup=parse(Float64, headerdata[4]), # Scale
-        αem=parse(Float64, headerdata[5]),     # AQEDUP
-        αs=parse(Float64, headerdata[6]),      # AQCDUP
+        nparticles=parse(Int16, headerdata[1]), # Number of particles
+        pid=parse(Int16, headerdata[2]),        # Process type
+        weight=parse(Float64, headerdata[3]),   # Event weight
+        scale=parse(Float64, headerdata[4]),    # Scale
+        aqed=parse(Float64, headerdata[5]),     # AQEDUP
+        aqcd=parse(Float64, headerdata[6]),     # AQCDUP
     )
     particles = [
         begin
             fields = split(line, ' '; keepempty=false)
             p = (;
-                particle=parse(Int32, fields[1]),
+                idx=idx-1, # zero-based to match `mother1`, `mother2`
+                id=parse(Int32, fields[1]),
                 status=parse(Int8, fields[2]),
-                mothup=(parse(UInt8, fields[3]), parse(UInt8, fields[4])),
-                color=(parse(UInt16, fields[5]), parse(UInt16, fields[6])),
-                p4=LorentzVector(
-                    parse(Float64, fields[10]),
-                    parse(Float64, fields[7]),
-                    parse(Float64, fields[8]),
-                    parse(Float64, fields[9]),
-                ),
+                mother1=parse(Int16, fields[3]),
+                mother2=parse(Int16, fields[4]),
+                color1=parse(Int32, fields[5]),
+                color2=parse(Int32, fields[6]),
+                px=parse(Float64, fields[7]),
+                py=parse(Float64, fields[8]),
+                pz=parse(Float64, fields[9]),
+                e=parse(Float64, fields[10]),
                 m=parse(Float64, fields[11]),
-                vtimup=parse(Float64, fields[12]),
-                spinup=parse(Float64, fields[13]),
+                lifetime=parse(Float64, fields[12]),
+                spin=parse(Float64, fields[13]),
             )
             p
-        end for line in lines[2:2+header.nup-1]
+        end for (idx,line) in enumerate(lines[2:2+header.nparticles-1])
     ]
-    return (; header=header, particles=particles)
+    return Event(header, particles)
 end
 
 function parse_lhe(filename)
